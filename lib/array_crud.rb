57a618ca01e202c761ccb543cf27abeda89ddefe
Kimberly Baker <bakerk2010@gmail.com>
2020-07-20 00:40:56 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,6 @@ end
 def add_element_to_end_of_array(array, element)
   pets = ["Dog", "Cat", "Fish", "Bird"]
   pets << "Turtle"
-  puts pets.inspect
 end
 
 def add_element_to_start_of_array(array, element)
