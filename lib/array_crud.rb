05f2b0f705d639bfbabb93f8b7264960de163adf
Kimberly Baker <bakerk2010@gmail.com>
2020-07-20 01:04:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,12 +7,10 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  ["Dog", "Cat", "Fish", "Bird"]
   push("Turtle")
 end
 
 def add_element_to_start_of_array(array, element)
-  pets = ["Dog", "Cat", "Fish", "Bird"]
   pets.unshift("Lizard")
 end
 
