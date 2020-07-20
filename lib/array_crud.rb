a052922422084752a0e60087399d0c3cb991ffdb
Kimberly Baker <bakerk2010@gmail.com>
2020-07-20 01:15:47 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,12 +27,12 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  pets = ["wow", "Dog", "Cat", "Fish", "Bird", "arrays!"]
+  pets = ["wow", "i", "am", "really", "learning", "arrays!"]
   [2]
 end
 
 def retrieve_first_element_from_array(array)
-  pets = ["wow", "Dog", "Cat", "Fish", "Bird", "arrays!"]
+  pets = ["wow", "i", "am", "really", "learning", "arrays!"]
   [0]
 end
 
